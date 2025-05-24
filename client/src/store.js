de6e974d5c1d147ab3e76608de8154b887732444
Sergi Jajanidze <sergi.jajanidze@gmail.com>
2025-05-24 13:30:28 +0400

    feat: persisted store with local storage

--- client/src/store.js
+++ client/src/store.js
@@ -1,29 +1,37 @@
 import { create } from "zustand";
+import { persist } from "zustand/middleware";
 
-const useAppStore = create((set) => ({
-  user: null,
-  setUser: (user) => set({ user }),
+const useAppStore = create(
+  persist(
+    (set) => ({
+      user: null,
+      setUser: (user) => set({ user }),
 
-  cities: [],
-  setCities: (cities) => set({ cities }),
+      cities: [],
+      setCities: (cities) => set({ cities }),
 
-  selectedCityId: "",
-  setSelectedCityId: (id) => set({ selectedCityId: id }),
+      selectedCityId: "",
+      setSelectedCityId: (id) => set({ selectedCityId: id }),
 
-  parkingAreas: [],
-  setParkingAreas: (areas) => set({ parkingAreas: areas }),
+      parkingAreas: [],
+      setParkingAreas: (areas) => set({ parkingAreas: areas }),
 
-  selectedParkingAreaId: "",
-  setSelectedParkingAreaId: (id) => set({ selectedParkingAreaId: id }),
+      selectedParkingAreaId: "",
+      setSelectedParkingAreaId: (id) => set({ selectedParkingAreaId: id }),
 
-  activeSession: null,
-  setActiveSession: (session) => set({ activeSession: session }),
+      activeSession: null,
+      setActiveSession: (session) => set({ activeSession: session }),
 
-  sessions: [],
-  setSessions: (sessions) => set({ sessions }),
+      sessions: [],
+      setSessions: (sessions) => set({ sessions }),
 
-  message: "",
-  setMessage: (message) => set({ message }),
-}));
+      message: "",
+      setMessage: (message) => set({ message }),
+    }),
+    {
+      name: "on-street-parking-storage",
+    }
+  )
+);
 
 export default useAppStore;
