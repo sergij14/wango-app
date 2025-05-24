6a8c1240ecd5df16e2500414a12ba411a78cf8d6
Sergi Jajanidze <sergi.jajanidze@gmail.com>
2025-05-24 13:12:34 +0400

    test:  created tests

---
+++ server/tests/api.test.js
@@ -0,0 +1,13 @@
+const request = require("supertest");
+const app = require("../index");
+
+test("Register and login", async () => {
+  const user = await request(app).post("/register").send({ email: "test@test.com", address: "123", carPlate: "ABC123" });
+  const login = await request(app).post("/login").send({ email: "test@test.com", carPlate: "ABC123" });
+  expect(login.statusCode).toBe(200);
+});
+
+test("Invalid login", async () => {
+  const login = await request(app).post("/login").send({ email: "not@found.com", carPlate: "XYZ" });
+  expect(login.statusCode).toBe(401);
+});
